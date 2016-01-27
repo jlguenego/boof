33d535fee33fb49f0e202a26bad034eec6e684a8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-01-27 17:07:37 +0100

    add title

--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('myApp', []);
+	var app = angular.module('mainApp', []);
+	
+	app.run(['$injector', function($injector) {
+		var $rootScope = $injector.get('$rootScope');
+		
+		
+		$rootScope.title = 'Boof ! Calorie Citron';
+
+	}]);
 
 })();
