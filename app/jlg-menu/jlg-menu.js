d8682dc348b21931ee298d5523465a988ec43792
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-02-02 15:39:41 +0100

    jlg-responsive

--- app/jlg-menu/jlg-menu.js
+++ app/jlg-menu/jlg-menu.js
@@ -1,7 +1,7 @@
 (function() {
 	'use strict';
 
-	var app = angular.module('jlg-menu', []);
+	var app = angular.module('jlg-menu', ['jlg-responsive']);
 	
 	app.run(['$injector', function($injector) {
 		var $rootScope = $injector.get('$rootScope');
@@ -9,12 +9,29 @@
 
 	}]);
 	
-	app.controller('jlg-menu.Ctrl', ['$injector', function($injector) {
+	app.controller('jlg-menu.Ctrl', ['$injector', '$scope', function($injector, $scope) {
+		var $templateRequest = $injector.get('$templateRequest');
+		var $compile = $injector.get('$compile');
 		
 		this.visible = false;
 		this.toggle = function(name) {
 			console.log('toggle');
 			this.visible = !this.visible;
+			if (!this.visible) {
+				return;
+			}
+			this.open('tmpl/test.html');
+		};
+		
+		this.open = function(templateUrl) {
+			var self = this;
+			$templateRequest(templateUrl).then(function(response) {
+				self.element.append(response);
+				console.log('response', self.element);
+				$compile(self.element.children())($scope);
+			}).catch(function(error) {
+				console.error('error', error);
+			});
 		};
 		
 	}]);
@@ -25,8 +42,10 @@
 
 		return {
 			restrict: 'EAC',
-			link: function(scope, element, attrs, ctrl) {
-				console.log('link jlgMenuLayer', ctrl);
+			link: function(scope, element, attrs) {
+				console.log('link jlgMenuLayer', scope);
+				var ctrl = scope[attrs.ctrl];
+				ctrl.element = element;
 			}
 		};
 	}]);
---
+++ app/jlg-responsive/jlg-responsive.js
@@ -0,0 +1,45 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('jlg-responsive', []);
+	
+	app.run(['$injector', function($injector) {
+		var $rootScope = $injector.get('$rootScope');
+		var $document = $injector.get('$document');
+		console.log('jlg-responsive: run');
+		$rootScope.isMobile = ('ontouchstart' in window);
+		var body = angular.element($document[0].body);
+		$rootScope.$watch('isMobile', function() {
+			body.removeClass('jlg-mobile jlg-desktop');
+			if ($rootScope.isMobile) {
+				body.addClass('jlg-mobile');
+			} else {
+				body.addClass('jlg-desktop');
+			}
+		});
+		
+	}]);
+	
+	app.controller('jlg-menu.Ctrl', ['$injector', function($injector) {
+		
+		this.visible = false;
+		this.toggle = function(name) {
+			console.log('toggle');
+			this.visible = !this.visible;
+		};
+		
+	}]);
+	
+	app.directive('jlgMenu', ['$injector', function($injector) {
+		var $templateRequest = $injector.get('$templateRequest');
+		var $compile = $injector.get('$compile');
+
+		return {
+			restrict: 'EAC',
+			link: function(scope, element, attrs, ctrl) {
+				console.log('link jlgMenuLayer', ctrl);
+			}
+		};
+	}]);
+
+})();
