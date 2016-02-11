bb8caaeeab53a656ec010fde7a789135b53bffd9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-02-11 15:54:55 +0100

    jlg-typeahead

--- app/jlg-typeahead/jlg-typeahead.js
+++ app/jlg-typeahead/jlg-typeahead.js
@@ -3,23 +3,35 @@
 
 	var app = angular.module('jlg-typeahead', ['jlg-responsive']);
 	
+	app.run(['$injector', function($injector) {
+		var $rootScope = $injector.get('$rootScope');
+		$rootScope.coucou = function($event) {
+			console.log('coucou');
+			$event.preventDefault();
+			return false;
+		};
+	}]);
 
 	
 	app.directive('jlgTypeahead', ['$injector', function($injector) {
-		var $templateRequest = $injector.get('$templateRequest');
 		var $compile = $injector.get('$compile');
-
+		var $q = $injector.get('$q');
+		
 		return {
 			restrict: 'EAC',
+			
 			link: function(scope, element, attrs) {
-				console.log('link jlgMenuLayer', scope, attrs);
-				var ctrl = scope[attrs.ctrl];
-				ctrl.element = element;
-				ctrl.initialPage = attrs.init;
-				ctrl.initialTitle = attrs.title;
-				ctrl.name = attrs.ctrl;
-				ctrl.initMenu();
+				console.log('link jlgTypeahead', scope, element, attrs);
+				var spec = scope.$eval(attrs.jlgTypeahead);
+				console.log('spec', spec);
+				var popup = angular.element('<div class="jlg-typeahead-popup"><button ng-click="coucou($event);">coucou</button></div>');
+				popup.append('<div ng-repeat="' + spec.select + '"><div>{{' + spec.title + '}}</div></div>');
+				console.log('popup', popup);
+				element.after(popup);
+				$compile(popup)(scope);
 			}
 		};
 	}]);
+	
+
 })();
