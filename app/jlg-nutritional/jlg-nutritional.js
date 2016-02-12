fd833b636bc3d449499bc0d4cd1d86979cccebcb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-02-12 16:06:28 +0100

    beautiful table

--- app/jlg-nutritional/jlg-nutritional.js
+++ app/jlg-nutritional/jlg-nutritional.js
@@ -10,18 +10,7 @@
 		console.log('jlg-nutritional: run');
 		$rootScope.nu = {};
 		$rootScope.nu.aliment = undefined;
-		$rootScope.nu.aliments = [
-			'Citron',
-			'Orange',
-			'Banane',
-			'Mangue',
-			'Concombre',
-			'Riz',
-			'Raisin',
-			'Cacao',
-			'Fraise',
-			'Pomme'
-		];
+		$rootScope.nu.aliments = [];
 		
 		$rootScope.searchForAliment = function() {
 			console.log('searchForAliment', $rootScope.nu.aliment);
@@ -47,17 +36,40 @@
 				beforeFirstChunk: undefined,
 				withCredentials: undefined
 			});
+			$rootScope.data.data = $rootScope.data.data.filter(function(n) { return n.ORIGFDNM != undefined });
 			
 			$rootScope.nu.aliments = $rootScope.data.data.map(function(n) { return n.ORIGFDNM; }).sort();
 
 			$rootScope.$watch('nu.aliment', function() {
-				$rootScope.nu.alimentData = $rootScope.data.data.find(function(n) { return n.ORIGFDNM == $rootScope.nu.aliment; });
+				$rootScope.nu.alimentData = $rootScope.data.data.find(function(n) { return n.ORIGFDNM === $rootScope.nu.aliment; });
+				console.log('$rootScope.nu.alimentData', $rootScope.nu.alimentData);
+				$rootScope.nu.alimentDataTable = object2array($rootScope.nu.alimentData);
 			});
 		}).catch(function(error) {
 			console.error('error', error);
 		});
 		
 		
+	}]);
+	
+	var object2array = function(object) {
+		var result = [];
+		for (var key in object) { 
+			result.push({'key': key, 'value': object[key]});	
+		}
+		return result.sort(function(a, b) { return a.key > b.key ? -1 : 1; });
+	};
+	
+	app.directive('jlgNutritionalTable', ['$injector', function($injector) {
+		var $compile = $injector.get('$compile');
+		var $q = $injector.get('$q');
+		console.log('jlgNutritionalTable start');
+		return {
+			restrict: 'EAC',
+			scope: true,
+			template: '<table ng-show="nu.alimentDataTable.length > 0" class="table-hover table-striped"><tr><th>Nutriment</th><th>Value</th></tr>' + 
+				'<tr ng-repeat="a in nu.alimentDataTable"><td>{{a.key}}</td><td>{{a.value}}</td></tr></table>'
+		};
 	}]);
 	
 	
