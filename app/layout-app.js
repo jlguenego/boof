6c0fa4842fcdc26427c4271c1974a2668f1f53ee
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-02-26 16:00:38 +0100

    directive jlg-layout

--- app/layout-app.js
+++ app/layout-app.js
@@ -9,5 +9,37 @@
 		
 		
 	}]);
+	
+	app.directive('jlgLayout', ['$injector', function($injector) {
+		return {
+			restrict: 'ECA',
+			require: 'jlgLayout',
+			scope: {
+				cfg: '=jlgLayout'
+			},
+			link: function(scope, element, attrs) {
+				console.log('jlgLayout', arguments);
+				
+			},
+			controller: ['$scope', '$injector', function ($scope, $injector) {
+				console.log('scope', $scope);
+				$scope.name = 'jlgLayoutCtrl';
+				this.cfg = $scope.cfg;
+			}]
+		};
+		
+	}]);
+	
+	app.directive('jlgLayoutContainer', ['$injector', function($injector) {
+		return {
+			restrict: 'ECA',
+			require: '^jlgLayout',
+			link: function(scope, element, attrs, ctrl) {
+				console.log('jlgLayoutContainer', arguments);
+				console.log('cfg of jlgLayout', ctrl.cfg);
+			}
+		};
+		
+	}]);
 
 })();
